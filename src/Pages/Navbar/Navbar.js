0d9c7d738bf7ef499b8c47f9d3fc557dc18af0e8
unknown <afia.nasrin3e@gmail.com>
2022-10-26 07:04:50 +0600

    fynaly footer section design are compleate

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -119,23 +119,13 @@ const Navbar = () => {
                         title="Company"
                         class="inline-flex items-center"
                       >
-                        <svg
-                          class="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
+                        <img
+                          className="w-10 rounded-full"
+                          src={navbar}
+                          alt=""
+                        />
                         <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Company
+                          P & A related Course
                         </span>
                       </a>
                     </div>
